acb08dfeb33d30b14efa11b4d36a5ae7e31e62c1
Aiur <diegomactor13@gmail.com>
2022-11-19 23:16:34 -0500

    update port 3

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const session = require("express-session");
+const env = require('dotenv');
+env.config()
 const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
